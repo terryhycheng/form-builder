3d5adb1ca867b18cd8352ec8309ca5bf12438d7f
Richard Griffiths <rich.griffiths89@gmail.com>
2023-11-24 17:50:15 +0000

    Made preview dialog full screen

--- components/PreviewModal.tsx
+++ components/PreviewModal.tsx
@@ -27,26 +27,34 @@ const PreviewComponent = () => {
                     Preview
                 </Button>
             </DialogTrigger>
-            <DialogContent className="max-w-2xl">
-                <DialogHeader>
+            <DialogContent className="w-screen h-screen max-h-screen max-w-full flex flex-col flex-grow p-0 gap-0">
+                <div className="px-4 py-2 border-b">
+                    <p className="text-lg font-bold text-muted-foreground">Form preview</p>
+                    <p className="text-sm text-muted-foreground">Preview your form before publishing</p>
+                </div>
+                {/* <DialogHeader>
                     <DialogTitle>Preview</DialogTitle>
                     <DialogDescription>Preview your form before publishing</DialogDescription>
-                </DialogHeader>
-                {!!fields.length &&
-                    fields.map((field) => (
-                        <div key={field.id} className="my-t w-full">
-                            <label htmlFor={slugify(field.label ?? '')} className="mb-2 inline-block">
-                                {field.label} {field.required && <span className="text-red-500">*</span>}
-                            </label>
-                            <Input
-                                type={field.type}
-                                id={slugify(field.label ?? '')}
-                                name={slugify(field.label ?? '')}
-                                placeholder={field.placeholder ?? ''}
-                                required={field.required}
-                            />
-                        </div>
-                    ))}
+                </DialogHeader> */}
+                <div className="bg-gray-100 dark:bg-slate-900 flex flex-col flex-grow items-center justify-center p-4 overflow-y-auto">
+                    <div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background h-full w-full rounded-2xl p-8 overflow-y-auto shadow">
+                        {!!fields.length &&
+                            fields.map((field) => (
+                                <div key={field.id} className="my-t w-full">
+                                    <label htmlFor={slugify(field.label ?? '')} className="mb-2 inline-block">
+                                        {field.label} {field.required && <span className="text-red-500">*</span>}
+                                    </label>
+                                    <Input
+                                        type={field.type}
+                                        id={slugify(field.label ?? '')}
+                                        name={slugify(field.label ?? '')}
+                                        placeholder={field.placeholder ?? ''}
+                                        required={field.required}
+                                    />
+                                </div>
+                        ))}
+                    </div>
+                </div>
             </DialogContent>
         </Dialog>
     );
