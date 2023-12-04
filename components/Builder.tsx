171f3f355d2391de622b9e0545c68c5c9d0568c3
Richard Griffiths <rich.griffiths89@gmail.com>
2023-12-04 15:52:05 +0000

    added some icons

--- components/Builder.tsx
+++ components/Builder.tsx
@@ -5,7 +5,7 @@ import React, { useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import FieldComponent from './Field';
 import { toast } from 'react-toastify';
-import { ChevronLeft, Eye, EyeOff, Rocket, Save, Trash2 } from 'lucide-react';
+import { Binary, CheckSquare2, ChevronDown, ChevronLeft, CircleDot, Heading2, Image, Rocket, Save, Text, TextSelect, Video, WholeWord, Image as ImageIcon, LayoutPanelTop } from 'lucide-react';
 import { $Enums, Field, Form } from '@prisma/client';
 import { useRouter } from 'next/navigation';
 import { FieldContext } from '@/contexts/FieldContext';
@@ -66,7 +66,7 @@ const Builder = ({ form }: BuilderProps) => {
 
     return (
         <>
-            <div className="w-full my-6 flex justify-start items-start gap-6">
+            <div className="w-full mt-10 mb-6 flex justify-start items-start gap-6">
                 <Button size={'icon'} variant={'secondary'} onClick={() => router.push('/dashboard')}>
                     <ChevronLeft className="w-4 h-4" />
                 </Button>
@@ -75,7 +75,7 @@ const Builder = ({ form }: BuilderProps) => {
                     <p className="text-md my-2 text-gray-600 dark:text-slate-500">{form?.description}</p>
                 </div>
             </div>
-            <section className="mb-4 flex justify-end items-center gap-2">
+            <section className="mb-8 flex justify-end items-center gap-2">
                 <PreviewComponent />
                 <Button onClick={saveFormHandler}>
                     <Save className="w-4 h-4 mr-2" />
@@ -89,8 +89,13 @@ const Builder = ({ form }: BuilderProps) => {
             </section>
             <section className="flex gap-4">
                 <div className="border flex-1 p-10 flex">
-                    <form action={() => {}} className="flex-1 flex justify-center items-center flex-col">
-                        {!fields.length && <p>There is no field at the moment :)</p>}
+                    <form action={() => {}} className="flex-1 flex justify-start items-center flex-col">
+                        {!fields.length && 
+                            <div className='h-full flex flex-col justify-center items-center gap-3'>
+                                <LayoutPanelTop className='w-6 h-6' />
+                                <p>There are no fields at the moment :)</p>
+                            </div>
+                        }
                         {fields.map((field) => (
                             <div key={field.id} className="border rounded-md mt-4 overflow-hidden w-full">
                                 <FieldComponent field={field} setFields={setFields} />
@@ -98,15 +103,48 @@ const Builder = ({ form }: BuilderProps) => {
                         ))}
                     </form>
                 </div>
-                <div className="border w-[30%] p-6">
-                    <div className="grid grid-cols-2 gap-4">
-                        <button className="border p-8 rounded-md" onClick={() => createField('TEXT')}>
-                            Text field
-                        </button>
-                        <button className="border p-8 rounded-md" onClick={() => createField('NUMBER')}>
-                            Number field
-                        </button>
+                <div className="border w-[30%] p-6 flex flex-col justify-start items-start gap-y-6">
+                    <div>
+                        <p className='mb-2'>Sections</p>
+                        <div className="grid grid-cols-2 gap-4">
+                            <button className="border p-6 rounded-md flex justify-center items-center gap-3 hover:bg-slate-100/50 dark:hover:bg-slate-900/50" onClick={() => createField('TEXT')}>
+                                <Heading2 className='w-4 h-4'/>Heading
+                            </button>
+                            <button className="border p-6 rounded-md flex justify-center items-center gap-3 hover:bg-slate-100/50 dark:hover:bg-slate-900/50">
+                                <Text className='w-4 h-4'/>Paragraph
+                            </button>
+                            <button className="border p-6 rounded-md flex justify-center items-center gap-3 hover:bg-slate-100/50 dark:hover:bg-slate-900/50" onClick={() => createField('NUMBER')}>
+                                <ImageIcon className='w-4 h-4'/>Image
+                            </button>
+                            <button className="border p-6 rounded-md flex justify-center items-center gap-3 hover:bg-slate-100/50 dark:hover:bg-slate-900/50" onClick={() => createField('NUMBER')}>
+                                <Video className='w-4 h-4'/>Video
+                            </button>
+                        </div>
                     </div>
+                    <div>
+                        <p className='mb-2'>Fields</p>
+                        <div className="grid grid-cols-2 gap-4">
+                            <button className="border p-6 rounded-md flex justify-center items-center gap-3 hover:bg-slate-100/50 dark:hover:bg-slate-900/50" onClick={() => createField('TEXT')}>
+                                <WholeWord className='w-4 h-4'/>Text
+                            </button>
+                            <button className="border p-6 rounded-md flex justify-center items-center gap-3 hover:bg-slate-100/50 dark:hover:bg-slate-900/50">
+                                <TextSelect className='w-4 h-4'/>Long Text
+                            </button>
+                            <button className="border p-6 rounded-md flex justify-center items-center gap-3 hover:bg-slate-100/50 dark:hover:bg-slate-900/50" onClick={() => createField('NUMBER')}>
+                                <Binary className='w-4 h-4'/>Number
+                            </button>
+                            <button className="border p-6 rounded-md flex justify-center items-center gap-3 hover:bg-slate-100/50 dark:hover:bg-slate-900/50" onClick={() => createField('NUMBER')}>
+                                <ChevronDown className='w-4 h-4'/>Select
+                            </button>
+                            <button className="border p-6 rounded-md flex justify-center items-center gap-3 hover:bg-slate-100/50 dark:hover:bg-slate-900/50" onClick={() => createField('NUMBER')}>
+                                <CheckSquare2 className='w-4 h-4'/>Checkbox
+                            </button>
+                            <button className="border p-6 rounded-md flex justify-center items-center gap-3 hover:bg-slate-100/50 dark:hover:bg-slate-900/50" onClick={() => createField('NUMBER')}>
+                                <CircleDot className='w-4 h-4'/>Radio
+                            </button>
+                        </div>
+                    </div>
+                    
                 </div>
             </section>
         </>
